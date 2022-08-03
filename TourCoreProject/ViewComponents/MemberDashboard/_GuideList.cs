d6ae7f394e9aceba64d9927591550845f2813465
Ersin Bakırcı <ebakircie@gmail.com>
2022-08-03 17:21:41 +0300

    .

---
+++ TourCoreProject/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TourCoreProject.ViewComponents.MemberDashboard
+{
+    public class _GuideList:ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = guideManager.GetAll();
+            return View(values);
+        }
+    }
+}
